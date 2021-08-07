17fcf951bef96977bffd29fa464dd1c9cc5c248d
Mehrez Kristou <anjin.kristou@gmail.com>
2021-08-07 22:33:09 +0200

    uri fix for postgres

--- app/config.py
+++ app/config.py
@@ -4,10 +4,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 import os
+import re
 from   decouple import config
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 class Config():
 
@@ -17,5 +21,5 @@ class Config():
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
     # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = True
