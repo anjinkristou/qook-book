22ed60242fc5d5a5ce6bbdae715d7cac7c94bcd0
Mehrez Kristou <anjin.kristou@gmail.com>
2021-08-11 11:09:40 +0200

    fixed login issue

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ def register_extensions(app):
     mg.init_app(app)
 
 def register_blueprints(app):
+    lm.login_view = "auth_blueprint.login"
     for module_name in ('auth', 'home', 'recipe'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
